a4a67466730dce0bf7fb3eb15b9b84045f0513b0
Selim Efe <uysalselimefe@gmail.com>
2026-01-13 21:27:55 +0300

    Implement logic to count ascending adjacent character pairs
    Added source code that iterates through the input string. It compares each character 'str[i]' with 'str[i+1]' and increments a counter if the subsequent character has a higher ASCII value.

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+        char str[99];
+    int i,len=0;
+
+    printf("str girin ");
+    scanf("%s",str);
+
+    i=0;
+
+    while(str[i]!='\0'){
+        len++;
+        i++;
+    }
+
+    int sum=0;
+
+    for(i=0;i<len;i++){
+        if(str[i+1]>str[i]){
+            sum++;
+        }
+    }
+
+    printf("%d",sum);
+}
